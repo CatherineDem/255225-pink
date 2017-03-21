366293763528fe4d1a44cc29c042898f0e4dfaac
Ekaterina Demina <katedisenko@mail.ru>
2017-03-21 11:44:44 +0800

    Коррект для Travis

--- js/map.js
+++ js/map.js
@@ -1,13 +1,13 @@
 ymaps.ready(init);
-var myMap, 
+var myMap,
     myPlacemark;
 
-function init(){ 
+function init(){
     myMap = new ymaps.Map("map", {
         center: [59.9362, 30.3211],
         zoom: 16,
         controls: []
-    }); 
+    });
     
     myPlacemark = new ymaps.Placemark([59.9365, 30.3208], {
         hintContent: 'PINK',
