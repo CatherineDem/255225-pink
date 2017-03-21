9e8e3433139cafbc459b2200c2d43e11c961bced
Ekaterina Demina <katedisenko@mail.ru>
2017-03-21 12:00:02 +0800

    Коррект для Travis #2

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ function init(){
         zoom: 16,
         controls: []
     });
-    
+
     myPlacemark = new ymaps.Placemark([59.9365, 30.3208], {
         hintContent: 'PINK',
         balloonContent: 'Приезжайте в гости'
@@ -18,7 +18,7 @@ function init(){
         iconImageSize: [35, 35],
         iconImageOffset: [0, 0]
     });
-    
+
     myMap.geoObjects.add(myPlacemark);
     myMap.behaviors.disable(['scrollZoom']);
 }
